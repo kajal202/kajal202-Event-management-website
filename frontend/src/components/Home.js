0cddc9c8b4f8930f842702b044163ded07dfb418
kajal202 <96862427+kajal202@users.noreply.github.com>
2025-02-14 20:10:47 +0530

    Update Home.js

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -25,13 +25,10 @@ const Home = () => {
 <section className="contact-us py-20 bg-slate-200 text-center text-black">
   <h2 className="text-4xl font-bold mb-4">Join Us in Our Mission</h2>
   <p className="mt-4 text-xl text-gray-400 font-semibold">Become part of our journey and help us create unforgettable events.</p>
-  
-  {/* Button with hover and transition effects */}
   <a 
     href="/contact-us" 
-    className="mt-6 inline-block bg-green-600 text-white py-3 px-10 rounded-full text-lg font-semibold hover:bg-green-500 transform transition duration-300 hover:scale-105 shadow-xl hover:shadow-2xl"
-  >
-    Contact Us
+    className="mt-6 inline-block bg-green-600 text-white py-3 px-10 rounded-full text-lg font-semibold hover:bg-green-500 transform transition duration-300 hover:scale-105 shadow-xl hover:shadow-2xl"> 
+      Contact Us
   </a>
 </section>
 
