2a00d78daf6a25486de79e241f6d6b4f4c64f905
kajal202 <96862427+kajal202@users.noreply.github.com>
2025-02-14 20:08:00 +0530

    Update SignIn.js

--- frontend/src/components/SignIn.js
+++ frontend/src/components/SignIn.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useLogin } from "../context/LoginContext";
 import { Link, useNavigate } from "react-router-dom";
-import { AiOutlineMail, AiOutlineLock } from "react-icons/ai"; // Import icons from react-icons
+import { AiOutlineMail, AiOutlineLock } from "react-icons/ai"; 
 
 function SignIn() {
   const navigate = useNavigate();
